bc8986960bc75d4c1c4a9df314d43b9bf8a9eac3
Yisus <jmercua322@g.educaand.es>
2024-06-22 21:02:39 +0200

    Prueba del bot funcionando sin cargar el modelo ia

--- chatbox_textbox.js
+++ chatbox_textbox.js
@@ -24,3 +24,53 @@ toggleChatButton.addEventListener('click', function () {
 });
 
 
+const $ = el => document.querySelector(el)
+
+const $form = $('form')
+const $input = $('form input')
+const $template = $('#mensaje-template')
+const $messages = $('ul')
+const $container = $('chatbox')
+const $button = $('button')
+
+$form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    // Formateamos la texto para quitar espacios en blancos del final y tabulaciones,...
+    const messageText = $input.value.trim()
+
+    if (messageText !== ''){
+        // Añadimos el mensaje en el DOM
+        $input.value = ''
+
+    }
+
+    //Usamos la función addMessage definida más abajo
+
+    addMessage(messageText, 'user')
+
+})
+
+function addMessage(text, sender) {
+    // Clonar el template. True para una copia profunda --> Copia los elementos de dentro del template: li, span, p ...
+    const clonedTemplate = $template.content.cloneNode(true)
+
+    // De este template clonado, necesitamos obtnener los elementos de dentro del template para trabajar con ellos
+    const $newMessage = clonedTemplate.querySelector('.mensaje')
+    const $who = $newMessage.querySelector('span')
+    const $text = $newMessage.querySelector('p')
+
+    $text.textContent = text
+    $who.textContent = sender === 'bot' ? 'GPT' : 'Tú'
+    // Añadimos la clase correspondiente del mensaje
+    $newMessage.classList.add(sender)
+
+    
+    //Incluir nuevos elementos al DOM
+    $messages.appendChild($newMessage)
+
+    //actualizar el scroll para adaptarse al nuevo contenido
+    //Iguala el scroll vertical (scrollTop) a la altura que tiene el scroll total de ese elemento (visible y no visible), elemento $container
+    $container.scrollTop = $container.scrollHeight
+
+}
+
